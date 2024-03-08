d6f75d195f3b6c88d3acb6ab461232be0bf4bb35
Patrick Carlos <patrickmcarlos@hotmail.com>
2024-03-07 23:57:46 -0800

    adds home page heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Patrick Carlos</h1>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="flex flex-row w-full justify-evenly">
+                <div className="p-12">
+                    <h1 className="text-6xl font-semibold">Patrick Carlos</h1>
+                    <p className="text-lg font-">
+                        Frontend Engineer @ <b>Amazon SageMaker</b>
+                    </p>
+                    <p className="text-lg font-thin">
+                        Previously @ <b>Motive.io</b>
+                    </p>
+                </div>
+                <div className="p-12">Some content here</div>
+            </div>
+        </main>
+    );
 }
